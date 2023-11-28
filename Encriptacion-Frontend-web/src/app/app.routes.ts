a3b5f2275ff1d4bfc0969d86d6f97f1b7544f9b6
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-11-27 20:54:28 -0500

    cambios en el front

--- Encriptacion-Frontend-web/src/app/app.routes.ts
+++ Encriptacion-Frontend-web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EncriptacionComponent } from './pages/encriptacion/encriptacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:EncriptacionComponent}
+];
