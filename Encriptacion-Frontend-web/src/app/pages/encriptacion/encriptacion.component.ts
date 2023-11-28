a7a3175b2b63efa0b411b0da4e14a8620dc80526
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-11-27 19:40:27 -0500

    cambios frontend

--- Encriptacion-Frontend-web/src/app/pages/encriptacion/encriptacion.component.ts
+++ Encriptacion-Frontend-web/src/app/pages/encriptacion/encriptacion.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-encriptacion',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FileUploadModule, HttpClientModule],
   templateUrl: './encriptacion.component.html',
   styleUrl: './encriptacion.component.css'
 })
 export class EncriptacionComponent {
-
+  
 }
