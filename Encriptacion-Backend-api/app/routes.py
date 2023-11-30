6586799f521afd39fdaf35832a31a6d46396f0d9
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-11-29 21:32:41 -0500

    Finalizacion antes de dockerizar

--- Encriptacion-Backend-api/app/routes.py
+++ Encriptacion-Backend-api/app/routes.py
@@ -10,24 +10,26 @@ def encriptar_rsa():
     file = request.files['file']
     texto = file.read().decode('utf-8')
     texto_encriptado = Encriptacion_RSA.encriptar(texto)
-    
-    return jsonify({"texto_encriptado": texto_encriptado})
+    texto_encriptado_str = ' '.join(map(str, texto_encriptado))
+    return jsonify({"texto_encriptado": texto_encriptado_str})
 
 @main.route('/desencriptar/rsa', methods=['POST'])
 def desencriptar_rsa():
     file = request.files['file']
     texto_encriptado_str = file.read().decode('utf-8')
     texto_encriptado = list(map(int, texto_encriptado_str.split()))
-
     texto_desencriptado = Encriptacion_RSA.desencriptar(texto_encriptado)
-    return jsonify({"texto_desencriptado": texto_desencriptado})
+    if isinstance(texto_desencriptado, str):
+        texto_desencriptado_str = texto_desencriptado
+    else:
+        texto_desencriptado_str = ''.join(map(chr, texto_desencriptado))
+    return jsonify({"texto_desencriptado": texto_desencriptado_str})
 
 @main.route('/encriptar/aes', methods=['POST'])
 def encriptar_aes():
     file = request.files['file']
     texto = file.read().decode('utf-8')
     texto_encriptado = Encriptacion_AES.encriptar(texto)
-    
     texto_encriptado_b64 = base64.b64encode(texto_encriptado).decode('utf-8')
     return jsonify({"texto_encriptado": texto_encriptado_b64})
 
@@ -35,9 +37,7 @@ def encriptar_aes():
 def desencriptar_aes():
     file = request.files['file']
     texto_encriptado_b64 = file.read().decode('utf-8')
-    
     texto_encriptado = base64.b64decode(texto_encriptado_b64)
-    
     texto_desencriptado = Encriptacion_AES.desencriptar(texto_encriptado)
     return jsonify({"texto_desencriptado": texto_desencriptado})
 
@@ -46,14 +46,11 @@ def descargar_texto():
     data = request.get_json()
     texto = data['texto']
     nombre_archivo = data['nombre_archivo']
-
     if not nombre_archivo.endswith('.txt'):
         nombre_archivo += '.txt'
-
     mem = BytesIO()
     mem.write(texto.encode('utf-8'))
     mem.seek(0)
-
     response = make_response(mem.getvalue())
     response.headers['Content-Disposition'] = f'attachment; filename={nombre_archivo}'
     response.mimetype = 'text/plain'
--- Encriptacion-Frontend-web/src/app/pages/encriptacion-ui/encriptacion-ui.component.ts
+++ Encriptacion-Frontend-web/src/app/pages/encriptacion-ui/encriptacion-ui.component.ts
@@ -7,10 +7,13 @@ import { EncriptarService } from 'src/app/services/encriptar.service';
   styleUrls: ['./encriptacion-ui.component.css']
 })
 export class EncriptacionUIComponent {
+
   constructor(private encriptar: EncriptarService){}
 
   fileToUpload: File | null = null;
   contenidoTxt: string = '';
+  resultadoTxt: string = '';
+  tipoProceso: string = '';
   fileName: string = '';
 
   handleFileInput(event: any) {
@@ -26,67 +29,47 @@ export class EncriptacionUIComponent {
     fileReader.readAsText(file); 
   }
 
-  handleTextareaInput(event: any) {
-    this.contenidoTxt = event.target.value;
+  procesarArchivo(accion: 'encriptarRSA' | 'desencriptarRSA' | 'encriptarAES' | 'desencriptarAES') {
+    if (!this.fileToUpload) return;
+  
+    this.tipoProceso = accion; 
+    const archivo = this.fileToUpload;
+    const accionMap = {
+      encriptarRSA: () => this.encriptar.encriptarRSA(archivo),
+      desencriptarRSA: () => this.encriptar.desencriptarRSA(archivo),
+      encriptarAES: () => this.encriptar.encriptarAES(archivo),
+      desencriptarAES: () => this.encriptar.desencriptarAES(archivo)
+    };
+  
+    accionMap[accion]().subscribe(
+      (response: any) => {
+        this.resultadoTxt = response.texto_encriptado || response.texto_desencriptado;
+        console.log(`${accion}:`, response);
+      },
+      error => console.error(error)
+    );
   }
-
-  encriptarRSA() {
-    if (this.fileToUpload) {
-      this.encriptar.encriptarRSA(this.fileToUpload).subscribe(
-        (response: any) => {
-          console.log('Encriptación RSA:', response);
-        },
-        error => console.error(error)
-      );
-    }
-  }
-
-  desencriptarRSA() {
-    if (this.fileToUpload) {
-      this.encriptar.desencriptarRSA(this.fileToUpload).subscribe(
-        (response: any) => {
-          console.log('Desencriptación RSA:', response);
-        },
-        error => console.error(error)
-      );
-    }
-  }
-
-  encriptarAES() {
-    if (this.fileToUpload) {
-      this.encriptar.encriptarAES(this.fileToUpload).subscribe(
-        (response: any) => {
-          console.log('Encriptación AES:', response);
-        },
-        error => console.error(error)
-      );
-    }
-  }
-
-  desencriptarAES() {
-    if (this.fileToUpload) {
-      this.encriptar.desencriptarAES(this.fileToUpload).subscribe(
-        (response: any) => {
-          console.log('Desencriptación AES:', response);
-        },
-        error => console.error(error)
-      );
-    }
-  }
-
+  
   descargarTxt() {
-    if (this.contenidoTxt) {
-      this.encriptar.generarTxt(this.contenidoTxt, 'archivo_descargado').subscribe(
+    if (this.resultadoTxt) {
+      const nombreArchivo = this.tipoProceso + '_descarga';
+      this.encriptar.generarTxt(this.resultadoTxt, nombreArchivo).subscribe(
         (data: Blob) => {
           const downloadURL = window.URL.createObjectURL(data);
           const link = document.createElement('a');
           link.href = downloadURL;
-          link.download = "archivo_descargado.txt";
+          link.download = nombreArchivo;
           link.click();
           window.URL.revokeObjectURL(downloadURL);
+          this.limpiarTextAreas();
         },
         error => console.error('Error al descargar el archivo:', error)
       );
     }
-  }  
+  } 
+
+  limpiarTextAreas() {
+    this.contenidoTxt = 'Contenido del .txt';
+    this.resultadoTxt = 'Resultado';
+  }
 }
